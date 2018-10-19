a67dc62f1547793add9cfbbbc0fc29c2f87776a2
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-10-18 19:21:31 -0700

    End of Lecture 9

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: "variables.env" });
+const Recipe = require("./models/Recipe");
+const User = require("./models/User");
+
+//bring in graphql-express middleware
+const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+//connects to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
+
+//init app
+const app = express();
+//create graphiql app
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+//connect schemas with graphql
+app.use(
+  "/graphql",
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User
+    }
+  })
+);
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
